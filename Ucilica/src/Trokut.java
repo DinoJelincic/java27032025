b8bcf7ee1a879632801e18f8d4bf65bff05b7845
DinoJelincic <djelincic00@gmail.com>
2025-03-27 19:13:17 +0100

    finished classes

--- Ucilica/src/Trokut.java
+++ Ucilica/src/Trokut.java
@@ -1,2 +1,48 @@
 public class Trokut {
+    private String naziv;
+    private double stranicaA;
+    private double stranicaB;
+    private double stranicaC;
+
+    public Trokut(String naziv, double stranicaA, double stranicaB, double stranicaC){
+        this.naziv = naziv;
+        this.stranicaA = stranicaA;
+        this.stranicaB = stranicaB;
+        this.stranicaC = stranicaC;
+    }
+
+    public String getNaziv(){
+        return naziv;
+    }
+    public double getStranicaA(){
+        return stranicaA;
+    }
+    public double getStranicaB(){
+        return stranicaB;
+    }
+    public double getStranicaC(){
+        return stranicaC;
+    }
+    public double getOpseg(){
+        return stranicaA + stranicaB + stranicaA;
+    }
+
+    public double getPovrsina(){
+        double abc = getOpseg()/2;
+        return Math.sqrt(abc*(abc - stranicaA)*(abc - stranicaB)*(abc - stranicaC));
+    }
+
+    public void setNaziv(String naziv){
+        this.naziv = naziv;
+    }
+    public void setStranicaA(double stranicaA){
+        this.stranicaA = stranicaA;
+    }
+    public void setStranicaB(double stranicaB){
+        this.stranicaB = stranicaB;
+    }
+    public void setStranicaC(double stranicaC){
+        this.stranicaC = stranicaC;
+    }
+
 }
--- Ucilica/src/Ucilica.java
+++ Ucilica/src/Ucilica.java
@@ -1,5 +1,8 @@
 public class Ucilica {
     public static void main (String [] args){
+        Trokut trokutA = new Trokut("trokutA", 3,4,5);
+        System.out.println("Opseg prvog trokuta je:" + trokutA.getOpseg());
+        System.out.println("Povrsina prvog trokuta je: " + trokutA.getPovrsina() );
 
     }
 }
